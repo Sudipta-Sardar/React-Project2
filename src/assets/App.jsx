1c87ca095b6c7323ea96ec2fb25a6767a11c6bb7
Sudipta Sardar <143907911+Sudipta-Sardar@users.noreply.github.com>
2023-10-10 20:48:51 +0530

    Add files via upload

---
+++ src/assets/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeroSection from '../components/Hero'
+import Navigation from '../components/Navigation'
+
+
+const App=()=> {
+  return (
+    <div>
+      <Navigation/>
+    <HeroSection/>
+    </div>
+  )
+}
+
+export default App
